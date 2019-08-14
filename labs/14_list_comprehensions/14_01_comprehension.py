32b551fa3f53397c3835544a7b36a55a0b0bb616
roblpm <roblpm@hotmail.com>
2019-08-14 12:03:35 +0100

    14_labs

--- labs/14_list_comprehensions/14_01_comprehension.py
+++ labs/14_list_comprehensions/14_01_comprehension.py
@@ -9,3 +9,9 @@ word = "CodingNomads"
 result_list = ['C', 'o', 'd', 'i', 'n', 'g', 'N', 'o', 'm', 'a', 'd', 's']
 
 '''
+# Tested 14-08-19
+
+result_list = []
+word = "CodingNomads"
+result_list = [letter for letter in word]
+print(result_list)
--- labs/14_list_comprehensions/14_02_positive.py
+++ labs/14_list_comprehensions/14_02_positive.py
@@ -3,5 +3,11 @@ Using list comprehension, create a list "positive" from the list
 "numbers" that contains only the positive numbers from the list "numbers".
 
 '''
+# Tested 14-08-19
 
+positive = []
 numbers = [5, -8, 3, 10, -19, -22, 44, 2, -1, 4, 42]
+
+positive = [mynum for mynum in numbers if mynum > 0]
+
+print(positive)
