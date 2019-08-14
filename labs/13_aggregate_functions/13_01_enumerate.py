b1cdc04e320ca4111f8fb6d6f15095a9a6051e5c
roblpm <roblpm@hotmail.com>
2019-08-14 11:16:27 +0100

    13_Labs

--- labs/13_aggregate_functions/13_01_enumerate.py
+++ labs/13_aggregate_functions/13_01_enumerate.py
@@ -1,3 +1,11 @@
 '''
 Demonstrate the use of the .enumerate() function.
 '''
+#Tested 14-08-19
+
+my_list = ["bob", "jim", "harry", "martha"]
+
+for index, item in enumerate(my_list):
+    print(f"This enumerate function make finding the index: {index}, and item: {item} really easy")
+
+
--- labs/13_aggregate_functions/13_02_sum.py
+++ labs/13_aggregate_functions/13_02_sum.py
@@ -2,3 +2,14 @@
 Write a simple aggregate function, sum(), that takes a list a returns the sum.
 
 '''
+#Tested 14-08-19
+
+def my_sum(my_list):
+    accum = 0
+    for item in my_list:
+        accum += item
+    return(accum)
+
+print(f"List is 1, 2, 3, 4")
+accum = my_sum([1, 2, 3, 4])
+print(f"Sum of list is: {accum}")
