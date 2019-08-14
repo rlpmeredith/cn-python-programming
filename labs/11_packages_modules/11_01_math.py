ae8b3237181604a304ab42247d12a22c8ed78b98
roblpm <roblpm@hotmail.com>
2019-08-14 10:22:08 +0100

    11_Labs

--- labs/11_packages_modules/11_01_math.py
+++ labs/11_packages_modules/11_01_math.py
@@ -2,3 +2,8 @@
 Demonstrate how to access and print the value of pi to the console.
 
 '''
+#Tested 14-08-19
+
+import math
+
+print(math.pi)
--- labs/11_packages_modules/11_02_explore.py
+++ labs/11_packages_modules/11_02_explore.py
@@ -3,3 +3,12 @@ Do some research on other popular python packages and what the are used for. Fee
 and play around a little.
 
 '''
+# Tested 14-08-2019
+import requests
+import numpy as np
+
+r = requests.get('https://github.com/timeline.json')
+print(r)
+
+a = np.arange(15).reshape(3, 5)
+print(a)
